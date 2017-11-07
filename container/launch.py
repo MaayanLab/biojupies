d492175a52aa70c026a3400a4a7552e274c5e5d7
denis-torre <denis.torre@mssm.edu>
2017-11-06 19:15:21 -0500

    Added shared volume for jupyter and downloader containers

---
+++ container/launch.py
@@ -0,0 +1,88 @@
+#############################################
+########## 1. Load Libraries
+#############################################
+from kubernetes import client, config
+config.load_kube_config()
+
+#############################################
+########## 2. Generate Deployment
+#############################################
+# Load Extension
+extension = client.ExtensionsV1beta1Api()
+
+# Create Deployment Object
+deployment = client.ExtensionsV1beta1Deployment()
+
+# Fill Required Fields (apiVersion, kind, metadata)
+deployment.api_version = "extensions/v1beta1"
+deployment.kind = "Deployment"
+deployment.metadata = client.V1ObjectMeta(name="notebook-generator-deployment", labels={"group": "notebook-generator"})
+
+# Add Spec
+deployment.spec = client.ExtensionsV1beta1DeploymentSpec()
+deployment.spec.replicas = 1
+
+# Add Pod Template
+deployment.spec.template = client.V1PodTemplateSpec()
+deployment.spec.template.metadata = client.V1ObjectMeta(labels={"app": "notebook-generator"})
+deployment.spec.template.spec = client.V1PodSpec()
+
+# Create Volume
+volume = client.V1Volume()
+volume.name = "notebook-volume"
+volume.emptyDir = client.V1EmptyDirVolumeSource()
+deployment.spec.template.spec.volumes = [volume]
+
+# Create Volume Mount
+volume_mount = client.V1VolumeMount()
+volume_mount.name = "notebook-volume"
+volume_mount.mount_path = "/notebooks"
+
+# Create Jupyter Server
+jupyter_container = client.V1Container()
+jupyter_container.name="notebook-generator-jupyter"
+jupyter_container.image="gcr.io/notebook-generator/notebook-generator-jupyter"
+jupyter_container.ports = [client.V1ContainerPort(container_port=8888, host_port=8888)]
+jupyter_container.volume_mounts = [volume_mount]
+
+# Create Notebook Downloader
+downloader_container = client.V1Container()
+downloader_container.name="notebook-generator-downloader"
+downloader_container.image="gcr.io/notebook-generator/notebook-generator-downloader"
+downloader_container.ports = [client.V1ContainerPort(container_port=5000, host_port=5000)]
+downloader_container.volume_mounts = [volume_mount]
+
+# Add Containers
+deployment.spec.template.spec.containers = [jupyter_container, downloader_container]
+
+# Create Deployment
+extension.create_namespaced_deployment(namespace="default", body=deployment)
+
+#############################################
+########## 3. Generate Service
+#############################################
+# Create API Endpoint and Resources
+api_instance = client.CoreV1Api()
+service = client.V1Service()
+
+# Fill Required Fields (apiVersion, kind, metadata)
+service.api_version = "v1"
+service.kind = "Service"
+service.metadata = client.V1ObjectMeta(name="notebook-generator-service", labels={"group": "notebook-generator"})
+
+# Add Spec
+service.spec = client.V1ServiceSpec()
+service.spec.selector = {"app": "notebook-generator".format(**locals())}
+
+# Create Ports
+jupyter_port = client.V1ServicePort(protocol="TCP", port=8888, target_port=8888)
+jupyter_port.name = "notebook-generator-jupyter"
+downloader_port = client.V1ServicePort(protocol="TCP", port=5000, target_port=5000)
+downloader_port.name = "notebook-generator-downloader"
+
+# Add Ports
+service.spec.ports = [jupyter_port, downloader_port]
+service.spec.type = "LoadBalancer"
+
+# Create Service
+api_instance.create_namespaced_service(namespace="default", body=service)
---
+++ container/notebook-generator-downloader/flask/static/py/NotebookGenerator.py
@@ -0,0 +1,93 @@
+#################################################################
+#################################################################
+############### Notebook Generator ##############################
+#################################################################
+#################################################################
+##### Author: Denis Torre
+##### Affiliation: Ma'ayan Laboratory,
+##### Icahn School of Medicine at Mount Sinai
+
+#################################################################
+#################################################################
+############### 1. Library Configuration ########################
+#################################################################
+#################################################################
+
+#############################################
+########## 1. Load libraries
+#############################################
+##### 1. Python modules #####
+import nbformat as nbf
+
+#################################################################
+#################################################################
+############### 1. Notebook Generation ##########################
+#################################################################
+#################################################################
+
+#############################################
+########## 1. Generate Notebook
+#############################################
+
+def GenerateNotebook(acc):
+
+	# Create new notebook
+	notebook = nbf.v4.new_notebook()
+
+	# Get Intro Text
+	intro_text = """
+# {acc} Analysis Notebook
+## Overview
+##### Introduction
+This Notebook contains an analysis of GEO dataset [{acc}](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={acc}).  It has been programmatically generated with the Jupyter Notebook Generator, available at the following [link](https://amp.pharm.mssm.edu/notebook-generator-web/notebook?acc={acc}).
+
+##### Sections
+The report is divided in two sections:
+
+1. *Data Processing*. This section covers the process of download and preprocessing of the dataset.
+2. *Data Analysis*. This section covers the application of scripts and computational tools to analyze the dataset.
+
+## 1. Data Processing
+Here we download the dataset from the ARCHS4 library, load data and metadata in pandas DataFrames.
+	""".format(**locals())
+
+	intro_analysis_text = """
+## 2. Data Analysis
+Here we analyze the processed dataset.
+
+##### PCA Plot
+First, we compute a PCA plot of the samples.
+	""".format(**locals())
+
+	heatmap_analysis_text = """
+##### Expression Heatmap
+Second, we display a heatmap of the top 500 most variable genes.
+	""".format(**locals())
+
+	coexpression_analysis_text = """
+##### Gene Coexpression Heatmap
+Third, we display a correlation heatmap of the top 500 most variable genes.
+	""".format(**locals())
+
+	# Load Libraries
+	notebook['cells'].append(nbf.v4.new_markdown_cell(intro_text))
+	notebook['cells'].append(nbf.v4.new_code_cell('# Import Modules\n%matplotlib inline\nimport archs4\nimport pca\nimport heatmap\nimport clustergrammer\nfrom plotly.offline import init_notebook_mode\ninit_notebook_mode()'))
+
+	# Fetch Data
+	notebook['cells'].append(nbf.v4.new_code_cell('# Get Dataset\nrawcount_dataframe, sample_metadata_dataframe = archs4.fetch_dataset("{acc}")\nrawcount_dataframe.head()'.format(**locals())))
+	notebook['cells'].append(nbf.v4.new_code_cell('# Show Metadata\nsample_metadata_dataframe'.format(**locals())))
+
+	# Add tools
+	notebook['cells'].append(nbf.v4.new_markdown_cell(intro_analysis_text))
+	notebook['cells'].append(nbf.v4.new_code_cell('# PCA\npca.display(rawcount_dataframe)'))
+	notebook['cells'].append(nbf.v4.new_markdown_cell(heatmap_analysis_text))
+	notebook['cells'].append(nbf.v4.new_code_cell('# Heatmap\nheatmap.display(rawcount_dataframe);'))
+	notebook['cells'].append(nbf.v4.new_markdown_cell(coexpression_analysis_text))
+	notebook['cells'].append(nbf.v4.new_code_cell('# Gene-gene Correlation Heatmap\nheatmap.display(rawcount_dataframe, correlation=True);'))
+	notebook['cells'].append(nbf.v4.new_code_cell('# Display Clustergrammer\nclustergrammer.display_clustergram(rawcount_dataframe)'))
+
+	# Get string
+	notebook_string = nbf.writes(notebook)
+
+	# Return string
+	return notebook_string
