11af93fe1fc9444dd0ea8ec583ce89ecaafab360
denis-torre <denis.torre@mssm.edu>
2018-03-27 17:06:32 -0400

    Added asterisk to significant genes in signature table

--- library/v0.5/analysis_tools/signature_table/signature_table.py
+++ library/v0.5/analysis_tools/signature_table/signature_table.py
@@ -34,11 +34,12 @@ def run(signature, signature_label=''):
 
 def plot(signature):
 	# signature.index = ['<a href="http://www.genecards.org/cgi-bin/carddisp.pl?gene={x}" target="_blank">{x}</a>'.format(**locals()) for x in signature.index] # human
-	signature.index = ['<a href="http://amp.pharm.mssm.edu/Harmonizome/gene/{x}" target="_blank">{x}</a>'.format(**locals()) for x in signature.index] # mouse
+	signature.index = ['<a href="http://amp.pharm.mssm.edu/Harmonizome/gene/'+x.upper()+'" target="_blank">'+x+'</a>' for x in signature.index] # mouse
+	signature.index = [index.replace('target="_blank">', 'target="_blank"><b>*').replace('</a>', '</b></a>') if rowData['adj.P.Val'] < 0.05 else index for index, rowData in signature.iterrows()] # mouse
 	signature.index.name = 'Gene'
 	signature['logFC'] = round(signature['logFC'], ndigits=2)
 	signature['AveExpr'] = round(signature['AveExpr'], ndigits=2)
-	html_table = signature.rename(columns={'gene_symbol': 'Gene', 'P.Value': 'P-value', 'adj.P.Val': 'FDR'}).drop(['t', 'B'], axis=1).sort_values('P-value').head(50).to_html(escape=False, classes='w-100')
+	html_table = signature.rename(columns={'gene_symbol': 'Gene', 'P.Value': 'P-value', 'adj.P.Val': 'FDR'}).drop(['t', 'B'], axis=1).sort_values('P-value').head(100).to_html(escape=False, classes='w-100')
 	html_results = '<div style="max-height: 200px; overflow-y: scroll;">{}</div>'.format(html_table)
 	display(HTML('<style>.w-100{width: 100%;}</style>'))
 	# display(qgrid.show_grid(signature.rename(columns={'gene_symbol': 'Gene', 'P.Value': 'P-value', 'adj.P.Val': 'FDR'}).drop(['t', 'B'], axis=1), grid_options={'maxVisibleRows': 4}))
