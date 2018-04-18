24831eb289f974c1f062d4669d0096b9932ff6be
denis-torre <denis.torre@mssm.edu>
2018-04-18 10:34:51 -0400

    Updating notebook Docker

--- docker/launch.py
+++ docker/launch.py
@@ -4,7 +4,7 @@
 #######################################################
 #######################################################
 ##### Handles the download of BioJupies notebooks in the Docker container.
-import os, urllib.request, urllib.parse
+import os, urllib.request, urllib.parse, json
 
 ##################################################
 ########## 1. Download Notebook
@@ -14,21 +14,22 @@ def download_notebook():
 
 	# Check environmentas variable for notebook UID
 	notebook_uid = os.environ.get('NOTEBOOK_UID')
-	print(notebook_uid)
 
 	# If exists
 	if notebook_uid:
 
-		# # Get URL
-		# notebook_url = 'http://localhost:5000/notebook-generator-website/api/notebook/'+notebook_uid
+		# Get Notebook Data URl
+		notebook_data_url = 'http://amp.pharm.mssm.edu/biojupies/api/notebook/'+notebook_uid
 
-		# # Get Data
-		# with urllib.request.urlopen(notebook_url) as response:
-		# 	notebook_data = json.loads(response.read())
-		notebook_url = urllib.parse.quote('https://storage.googleapis.com/jupyter-notebook-generator/4JetkKyEu/GSE75070 Analysis Notebook.ipynb', safe=':/')
+		# Get Notebook Data
+		with urllib.request.urlopen(notebook_data_url) as response:
+			notebook_data = json.loads(response.read())
 			
+		# Get Notebook URL
+		notebook_url = urllib.parse.quote(notebook_data['notebook_url'], safe=':/')
+		print(notebook_url)
+
 		# Get data
-		# os.system('wget {notebook_url}'.format(**notebook_data))
 		os.system('wget '+notebook_url)
 
 		# Jupyter Trust
