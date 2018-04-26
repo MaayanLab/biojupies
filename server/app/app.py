665d34eaa6ee68d46b160914404c2ae15d73e918
denis-torre <denis.torre@mssm.edu>
2018-04-26 18:09:57 -0400

    Developing JSON tool annotation script

--- server/app/app.py
+++ server/app/app.py
@@ -36,7 +36,7 @@ from NotebookManager import *
 #############################################
 ##### 1. Flask App #####
 # General
-dev = True
+dev = False
 entry_point = '/notebook-generator-server-dev' if dev else '/notebook-generator-server'
 app = Flask(__name__, static_url_path=os.path.join(entry_point, 'app/static'))
 
