a92b8ac7943f954aebdb9f877fea753118da7456
denis-torre <denis.torre@mssm.edu>
2018-11-14 14:54:01 -0500

    Encode FASTQ file names with special characters

--- website/app/app.py
+++ website/app/app.py
@@ -1088,7 +1088,8 @@ def launch_alignment_api():
 			params = '&'.join(['{key}={value}'.format(**locals()) for key, value in sample.items() if value])+'&organism='+alignment_settings['organism']
 			url = "https://amp.pharm.mssm.edu/cloudalignment/createjob?username={ELYSIUM_USERNAME}&password={ELYSIUM_PASSWORD}&".format(**os.environ)+params
 			# Launch alignment jobs
-			req =  urllib.request.Request(url.replace(' ', '%20'))
+			req =  urllib.request.Request(urllib.parse.quote(url, safe=':/&.?='))
+			# req =  urllib.request.Request(url.replace(' ', '%20'))
 			resp = urllib.request.urlopen(req).read().decode('utf-8')
 			print(resp)
 
